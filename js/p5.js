01e340fa1f678400b37878ee64929ce92ff3c3a1
nonof1 <60931115+nonof1@users.noreply.github.com>
2020-03-17 16:28:19 +0100

    17.03.20 / MouseX/Y draws on page. Need a fix

--- js/p5.js
+++ js/p5.js
@@ -1,10 +1,11 @@
 var canvas;
 
 function setup() {
+    background();
     canvas = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     fill(0);
-    ellipse(300, 300, 300, 300);
+    ellipse(mouseX, mouseY, 30, 30);
 }
